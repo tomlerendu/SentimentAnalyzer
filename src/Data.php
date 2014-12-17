a04644adb0c25903b8fe1b71330172f486cdfb75
Tom Lerendu <tom.lerendu@gmail.com>
2014-12-17 19:31:02 +0000

    Start on data class

---
+++ src/Data.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace TweetSentiment;
+
+
+class Data
+{
+    private $pos = [];
+    private $neg = [];
+    
+    public function __construct($dataDir)
+    {
+
+    }
+
+    private function loadDataDir($dataDir)
+    {
+
+    }
+} 
