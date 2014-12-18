24916128e16d050a7c8a8427ab883eb7bb3fec1c
Tom Lerendu <tom.lerendu@gmail.com>
2014-12-18 13:22:23 +0000

    Add methods to get pos and neg count

--- src/Data.php
+++ src/Data.php
@@ -17,4 +17,24 @@ class Data
     {
 
     }
+
+    /**
+     * Finds the number of occurrences of a word in the positive data set
+     * @param $word - The word that is being counted
+     * @return int The number of occurrences of $word
+     */
+    public function getPosCount($word)
+    {
+        return isset($pos[$word]) ? $pos[$word] : 0;
+    }
+
+    /**
+     * Finds the number of occurrences of a word in the negative data set
+     * @param $word - The word that is being counted
+     * @return int The number of occurrences of $word
+     */
+    public function getNegCount($word)
+    {
+        return isset($neg[$word]) ? $neg[$word] : 0;
+    }
 } 
