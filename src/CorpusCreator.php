bac9a3f0aa7672a6526bfc7bca640f2d8f571fde
Tom Lerendu <tom.lerendu@gmail.com>
2014-12-20 00:20:06 +0000

    Start on Corpus constructor

---
+++ src/CorpusCreator.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace SentimentAnalyzer;
+
+class CorpusCreator
+{
+    private $corpus;
+
+    public function __construct($location)
+    {
+
+    }
+
+    public function addPositiveText($text)
+    {
+
+    }
+
+    public function addNegativeText($text)
+    {
+
+    }
+
+    public function saveTo($location)
+    {
+
+    }
+} 
